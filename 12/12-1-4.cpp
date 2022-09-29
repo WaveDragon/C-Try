b9a21fba763dd30dd9c3e083453389d54c16b9f8
MyownComputer <543836278@qq.com>
2022-09-29 10:42:34 +0800

    9-29

---
+++ 12/12-1-4.cpp
@@ -0,0 +1,55 @@
+#include "INCL.H"
+#include <memory>
+
+using namespace std;
+struct destination
+{
+};
+struct connection
+{
+    /* data */
+};
+connection connect(destination *pd)
+{
+    cout << "Open the linajie" << endl;
+    return connection();
+
+}
+void disconnect (connection c)
+{
+    cout << "Close the lianjie" << endl;
+}
+
+//未使用shared_ptr版本
+void f(destination &d)
+{
+    cout << "直接管理connect" << endl;
+    connection c = connect(&d);
+    //忘记调用disconnect 关闭连接
+    cout << endl;
+}
+
+void end_connection(connection *p)
+{
+    disconnect(*p);
+}
+
+
+//使用 shared_ptr版本
+void f1(destination &d)
+{
+    cout << "用shared_ptr管理 connect" << endl;
+    connection c = connect(&d);
+
+    shared_ptr<connection> p (&c,end_connection);
+
+    cout << endl;
+}
+
+int main()
+{
+    destination d;
+    f(d);
+    f1(d);
+    return 0;
+}
